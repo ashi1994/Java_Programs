206e21263917dcc1774879ed06acb8bb403ab37b
ashi1994 <ashiwaniranjan@gmail.com>
2018-08-01 10:41:02 +0530

    Added some functions

--- src/String/StringToInt.java
+++ src/String/StringToInt.java
@@ -4,18 +4,25 @@ class StringToInt
  public static void main(String args[])
    {
  
- //To convert a srting into a int, use:
+ //To convert a string into a int, use:
  String str = "1234";
 int i = Integer.parseInt(str);
  System.out.println("i: " + i);
+ 
+ //To convert a string into int
+ String s12="12445";
+ int k=Integer.valueOf(s12);
+ System.out.println(k+1);
  
  //To convert a number into a string, use:
 int num = 1234;   
 String str1 = String.valueOf(num);
 System.out.println(str1);
 
-//to convert primitive to string
+//to convert any primitive data type to string
 int a=10;
 String str2=Integer.toString(a);
 System.out.println(str2+10);
-}}
+}
+ 
+   }
